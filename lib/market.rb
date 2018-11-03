8d35a3960fff92695087ef93348ee7fbd8d687ab
Mackenzie-Frey <mackenziefrey1@gmail.com>
2018-11-03 17:46:58 -0600

    Working on final sell method of iteration 4

--- lib/market.rb
+++ lib/market.rb
@@ -9,4 +9,65 @@ class Market
     @vendors = []
   end
 
+  def add_vendor(vendor)
+    vendors << vendor
+  end
+
+  def vendor_names
+    @vendors.map do |vendor|
+      vendor.name
+    end
+  end
+
+  # def vendors_that_sell(query_item)
+  #   available_vendors = []
+  #   @vendors.each do |vendor|
+  #     vendor.inventory.each do |item_in_inventory|
+  #       if item_in_inventory[0] == query_item
+  #         available_vendors << vendor
+  #       end
+  #     end
+  #   end
+  #   available_vendors
+  # end
+
+  def vendors_that_sell(query_item)
+    @vendors.find_all do |vendor|
+      vendor.inventory.keys.include?(query_item)
+    end
+  end
+
+  def sorted_item_list
+    sorted_items = []
+    @vendors.each do |vendor|
+      sorted_items << vendor.inventory.keys
+    end
+    sorted_items.flatten.uniq.sort
+  end
+
+  def total_inventory
+    inventory_total = Hash.new(0)
+    @vendors. each do |vendor|
+      vendor.inventory.each do |item, quantity|
+        inventory_total[item] += quantity
+      end
+    end
+    inventory_total
+  end
+
+  def sell(item_requested, quantity_requested)
+    @vendors.each do |vendor|
+      vendor.inventory.each do |vendor_item, vendor_quantity|
+        if vendor_item == item_requested
+          difference = vendor_quantity - quantity_requested
+
+          if difference < 0
+            
+          end
+        end
+        binding.pry
+      end
+    end
+  end
+
 end
--- test/market_test.rb
+++ test/market_test.rb
@@ -16,5 +16,136 @@ class MarketTest < Minitest::Test
     assert_equal [], market.vendors
   end
 
+  def test_it_can_add_vendors
+    market = Market.new("South Pearl Street Farmers Market")
 
+    vendor_1 = Vendor.new("Rocky Mountain Fresh")
+    vendor_1.stock("Peaches", 35)
+    vendor_1.stock("Tomatoes", 7)
+    vendor_2 = Vendor.new("Ba-Nom-a-Nom")
+
+    vendor_2.stock("Banana Nice Cream", 50)
+    vendor_2.stock("Peach-Raspberry Nice Cream", 25)
+    vendor_3 = Vendor.new("Palisade Peach Shack")
+
+    vendor_3.stock("Peaches", 65)
+    market.add_vendor(vendor_1)
+    market.add_vendor(vendor_2)
+    market.add_vendor(vendor_3)
+    assert_equal [vendor_1, vendor_2, vendor_3], market.vendors
+  end
+
+  def test_vendors_have_names
+    market = Market.new("South Pearl Street Farmers Market")
+
+    vendor_1 = Vendor.new("Rocky Mountain Fresh")
+    vendor_1.stock("Peaches", 35)
+    vendor_1.stock("Tomatoes", 7)
+    vendor_2 = Vendor.new("Ba-Nom-a-Nom")
+
+    vendor_2.stock("Banana Nice Cream", 50)
+    vendor_2.stock("Peach-Raspberry Nice Cream", 25)
+    vendor_3 = Vendor.new("Palisade Peach Shack")
+
+    vendor_3.stock("Peaches", 65)
+    market.add_vendor(vendor_1)
+    market.add_vendor(vendor_2)
+    market.add_vendor(vendor_3)
+    expected = ["Rocky Mountain Fresh", "Ba-Nom-a-Nom", "Palisade Peach Shack"]
+    assert_equal expected, market.vendor_names
+  end
+
+  def test_it_finds_vendors_with_specific_items
+    market = Market.new("South Pearl Street Farmers Market")
+
+    vendor_1 = Vendor.new("Rocky Mountain Fresh")
+    vendor_1.stock("Peaches", 35)
+    vendor_1.stock("Tomatoes", 7)
+    vendor_2 = Vendor.new("Ba-Nom-a-Nom")
+
+    vendor_2.stock("Banana Nice Cream", 50)
+    vendor_2.stock("Peach-Raspberry Nice Cream", 25)
+    vendor_3 = Vendor.new("Palisade Peach Shack")
+
+    vendor_3.stock("Peaches", 65)
+    market.add_vendor(vendor_1)
+    market.add_vendor(vendor_2)
+    market.add_vendor(vendor_3)
+    assert_equal [vendor_1, vendor_3], market.vendors_that_sell("Peaches")
+    assert_equal [vendor_2], market.vendors_that_sell("Banana Nice Cream")
+  end
+
+  def test_it_sorts_items
+    market = Market.new("South Pearl Street Farmers Market")
+
+    vendor_1 = Vendor.new("Rocky Mountain Fresh")
+
+    vendor_1.stock("Peaches", 35)
+    vendor_1.stock("Tomatoes", 7)
+    vendor_2 = Vendor.new("Ba-Nom-a-Nom")
+
+    vendor_2.stock("Banana Nice Cream", 50)
+    vendor_2.stock("Peach-Raspberry Nice Cream", 25)
+    vendor_3 = Vendor.new("Palisade Peach Shack")
+
+    vendor_3.stock("Peaches", 65)
+    market.add_vendor(vendor_1)
+    market.add_vendor(vendor_2)
+    market.add_vendor(vendor_3)
+    expected = ["Banana Nice Cream", "Peach-Raspberry Nice Cream", "Peaches", "Tomatoes"]
+    assert_equal expected, market.sorted_item_list
+  end
+
+  def test_it_has_total_inventory
+    market = Market.new("South Pearl Street Farmers Market")
+
+    vendor_1 = Vendor.new("Rocky Mountain Fresh")
+
+    vendor_1.stock("Peaches", 35)
+    vendor_1.stock("Tomatoes", 7)
+    vendor_2 = Vendor.new("Ba-Nom-a-Nom")
+
+    vendor_2.stock("Banana Nice Cream", 50)
+    vendor_2.stock("Peach-Raspberry Nice Cream", 25)
+    vendor_3 = Vendor.new("Palisade Peach Shack")
+
+    vendor_3.stock("Peaches", 65)
+    market.add_vendor(vendor_1)
+    market.add_vendor(vendor_2)
+    market.add_vendor(vendor_3)
+    expected = {"Peaches"=>100, "Tomatoes"=>7, "Banana Nice Cream"=>50, "Peach-Raspberry Nice Cream"=>25}
+    assert_equal expected, market.total_inventory
+  end
+
+  def test_it_can_sell
+    market = Market.new("South Pearl Street Farmers Market")
+
+    vendor_1 = Vendor.new("Rocky Mountain Fresh")
+
+    vendor_1.stock("Peaches", 35)
+    vendor_1.stock("Tomatoes", 7)
+    vendor_2 = Vendor.new("Ba-Nom-a-Nom")
+
+    vendor_2.stock("Banana Nice Cream", 50)
+    vendor_2.stock("Peach-Raspberry Nice Cream", 25)
+    vendor_3 = Vendor.new("Palisade Peach Shack")
+
+    vendor_3.stock("Peaches", 65)
+    market.add_vendor(vendor_1)
+    market.add_vendor(vendor_2)
+    market.add_vendor(vendor_3)
+    refute market.sell("Peaches", 200)
+    skip
+    refute market.sell("Onions", 1)
+    assert market.sell("Banana Nice Cream", 5)
+    assert_equal 45, vendor_2.check_stock("Banana Nice Cream")
+    assert market.sell("Peaches", 40)
+    assert_equal 0, vendor_1.check_stock("Peaches")
+    assert_equal 60, vendor_3.check_stock("Peaches")
+  end
+
+  def test_can_check_stock
+
+
+  end
 end
