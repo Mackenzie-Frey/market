0490050a472c20b684f454240a5e9524a913f369
Mackenzie-Frey <mackenziefrey1@gmail.com>
2018-11-03 15:31:13 -0600

    iteration 2 setup

---
+++ lib/market.rb
@@ -0,0 +1,12 @@
+require './lib/vendor'
+
+class Market
+
+  attr_reader :name, :vendors
+
+  def initialize(name)
+    @name = name
+    @vendors = []
+  end
+
+end
---
+++ test/market_test.rb
@@ -0,0 +1,20 @@
+require 'minitest/pride'
+require 'minitest/autorun'
+require './lib/market'
+require 'pry'
+
+class MarketTest < Minitest::Test
+
+  def test_it_exists
+    market = Market.new("South Pearl Street Farmers Market")
+    assert_instance_of Market, market
+  end
+
+  def test_it_has_attributes
+    market = Market.new("South Pearl Street Farmers Market")
+    assert_equal "South Pearl Street Farmers Market", market.name
+    assert_equal [], market.vendors
+  end
+
+
+end
